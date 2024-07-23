6d85458869db0784d7226d6bdc100127cb36f1a5
Soo <iam6ear.file@gmail.com>
2024-07-23 10:57:35 +0900

    personal review ...ing

--- front/src/pages/board/PersonalReview.jsx
+++ front/src/pages/board/PersonalReview.jsx
@@ -3,7 +3,11 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { getUser } from './../../util/localStorage';
 
+// svg
+import {ReactComponent as IconClose} from "../../svg/icon-close-x.svg";
+
 export default function PersonalReview() {
+  const [target, setTarget] = useState();
   const [list, setList] = useState([]);
   const userId = getUser() ? getUser().userId : "test_soo";
   const data = {
@@ -16,6 +20,12 @@ export default function PersonalReview() {
     axios.post(url, data)
       .then(result => setList(result.data))
   },[])
+
+  const popupOpen = (id) => {
+    document.querySelector(".popup_wrap").classList.add("on");
+    document.querySelector("body").classList.add("popup");
+    setTarget(id)
+  }
 
   return (
     <div className='personalReview'>
@@ -32,23 +42,71 @@ export default function PersonalReview() {
                 {
                   Boolean(v.isReviewed)
                   ? <button type="button">리뷰읽기</button>
-                  : <button type="button">리뷰쓰기</button>
+                  : <button type="button" onClick={()=>popupOpen(v.orderId)}>리뷰쓰기</button>
                 }
               </div>
             ))
           }
         </div>
       </div>
-      <popupWrite />
+      <PopupWrite orderId={target}/>
     </div>
   );
 }
 
-function popupWrite () {
+function PopupWrite ({orderId}) {
+  const initData = {
+    orderId,
+    title : "",
+    content : "",
+  }
+  console.log(initData);
+  const [data, setData] = useState(initData);
+
+  const changeHandler = (e) => {
+    const [name, value] = e.target;
+    setData(prev => ({...prev, [name] : value}))
+  }
+
+  const popupClose = () => {
+    document.querySelector(".popup_wrap").classList.remove("on");
+    document.querySelector("body").classList.remove("popup");
+    setData(initData)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="popup_wrap">
+    <form className="popup_wrap" onSubmit={submitHandler}>
       <div className="bg"></div>
-      <div className="popup"></div>
-    </div>
+      <div className="popup">
+        <div className="popup_top">
+          <h3>수강 후기 등록</h3>
+          <button type="button" onClick={popupClose}><IconClose /></button>
+        </div>
+        <div className="popup_content_wrap">
+          <div className="scroll_wrap">
+            <div className="popup_content">
+              <ul className="data_section">
+                <li>
+                  <h4>제목</h4>
+                  <input type="text" name="title" value={data.title} placeholder="제목을 입력해 주세요." onChange={changeHandler}/>
+                </li>
+                <li>
+                  <h4>문의 내용</h4>
+                  <textarea name="content" value={data.content} onChange={changeHandler} placeholder="문의 내용을 입력해주세요."></textarea>
+                </li>
+              </ul>
+              <div className="btns">
+                <button type="button" className="basic_btn" onClick={popupClose}>취소하기</button>
+                <button className="basic_btn black">등록하기</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   );
 }
