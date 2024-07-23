81d6adb7bc312674174e23d26bf98009984e4da8
Soo <iam6ear.file@gmail.com>
2024-07-23 10:09:12 +0900

    personalReview ...ing

--- front/src/pages/board/PersonalReview.jsx
+++ front/src/pages/board/PersonalReview.jsx
@@ -1,9 +1,10 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { getUser } from './../../util/localStorage';
 
 export default function PersonalReview() {
+  const [list, setList] = useState([]);
   const userId = getUser() ? getUser().userId : "test_soo";
   const data = {
     userId,
@@ -13,7 +14,7 @@ export default function PersonalReview() {
     const url = "//localhost:8080/board/myReview"
 
     axios.post(url, data)
-      .then(result => console.log(result.data))
+      .then(result => setList(result.data))
   },[])
 
   return (
@@ -23,9 +24,31 @@ export default function PersonalReview() {
       </div>
       <div className="narrow_page">
         <div className="min_inner">
-          ...
+          {
+            list.map(v => (
+              <div className="box">
+                {v.order_no}
+                {v.course_name}
+                {
+                  Boolean(v.isReviewed)
+                  ? <button type="button">리뷰읽기</button>
+                  : <button type="button">리뷰쓰기</button>
+                }
+              </div>
+            ))
+          }
         </div>
       </div>
+      <popupWrite />
+    </div>
+  );
+}
+
+function popupWrite () {
+  return (
+    <div className="popup_wrap">
+      <div className="bg"></div>
+      <div className="popup"></div>
     </div>
   );
 }
